ee0919d9fb53a1a937977e277b9c66714b6de5e1
Roman <romanhassan328@gmail.com>
2025-08-14 20:49:43 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,35 @@
+/**
+ *  VARIABLE ==> Container
+ */
+
+// Number Type Variable
+var sunglassPrice = 100;
+
+// String Type Variable
+var subject = 'CSE';
+
+// Boolean Type Variable
+var passed = true;
+
+/**
+ * NAMING CONVENTION
+ * 1. single word
+ * 2. no quotation
+ * 3. no gap or dash
+ * 4. can not start with number
+ * 5. can not be a keyword
+ * 6. prefer camelCase
+ */
+
+var lastExamResult = 90;
+
+/**
+ * OPERATION 
+ * 1. +, -, *, /, %   .
+ * 2. +=, -=, *=, /=  .
+ * 3. isNaN ?
+ * 4. null  .
+ * 5. Number  .
+ */
+
+
