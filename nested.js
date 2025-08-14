b7e0e0d260fd3ea87926c58181112b259758ad69
Roman <romanhassan328@gmail.com>
2025-08-15 01:44:17 +0600

    added nested

---
+++ nested.js
@@ -0,0 +1,17 @@
+const money = -20;
+if (money > 300){
+    console.log('Bro, you are rich!!');
+}
+else{
+    if (money > 100){
+        console.log('Bro, you are middle class');
+    }
+    else{
+        if ( money > 0){
+        console.log('Some banana you can eat and earn some calcium');
+    }
+    else {
+        console.log('You are not my friend')
+    }
+    }
+}
