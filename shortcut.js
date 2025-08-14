780ae23da553a70d9c963e07704be4524ca5edb5
Roman <romanhassan328@gmail.com>
2025-08-15 02:56:24 +0600

    shortcut

---
+++ shortcut.js
@@ -0,0 +1,18 @@
+const isLeader = true;
+// if (isLeader === true){
+//  if (isLeader){
+//     console.log('leader come and sit in a chair')
+// }
+// else{
+//     console.log('Leader do not come no sit in a chair ')
+// }
+
+const isPassed = false;
+
+// if (isPassed === false)
+if (!isPassed){
+    console.log('We will marry you off');
+}
+else{
+    console.log('We will not marry you off.');
+}
