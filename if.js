78095105837b41a5ce67b6b8475b5f88e3fc810e
Roman <romanhassan328@gmail.com>
2025-08-14 22:03:06 +0600

    added if else statement

---
+++ if.js
@@ -0,0 +1,57 @@
+/***************************************
+ *  Tui Jodi Amake Khayas
+ * tahole ami asbo
+ * 
+ * -------------------------------------
+ * if you buy food for me 
+ *       i will come
+ * 
+ * --------------------------------------
+ * if Briyani price less than 300 taka
+ *              i will buy.
+ * 
+ * ------------STRUCTURE-----------------
+ *  if (condition){
+ * 
+ * }
+ * 
+ * --------------------------------------
+ *  if (condition){
+ * 
+ * }
+ * 
+ ***************************************/
+
+//---------------------------------------
+
+/*if (13 < 10){
+    console.log('I have a smaller value');
+}
+else{
+    console.log('I have not smaller value');
+}*/
+
+// var biryaniPrice = 280;
+// if (biryaniPrice > 400);
+// console.log('i will eaten biryani');
+
+/**
+ * if (condition){
+ *       execute code if condition above is true
+ * }
+ * else {
+ *     execute code if the condition is false
+ * }
+ */
+
+var weight = 20;
+
+if (weight <= 20){
+    console.log('I will carry it by myself');
+}
+/*else if ( weight == 20) {
+    console.log('I will half road carry it myself and half road rent a rickshaw');   
+} */
+else{
+    console.log('I will rent a rickshaw');
+}
