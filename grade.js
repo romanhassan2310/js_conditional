73b3f166821bdc613b1b2548b8e03cb64970b3b7
Roman <romanhassan328@gmail.com>
2025-08-14 23:59:22 +0600

    added grade

---
+++ grade.js
@@ -0,0 +1,23 @@
+const marks = 69;
+
+if ( marks >= 40  && marks <= 49){
+    const gradeLetter = 'C';
+    console.log(gradeLetter)
+}
+else if ( marks >= 50 && marks <= 59){
+    const gradeLetter = 'B';
+    console.log(gradeLetter)
+}
+else if ( marks >= 60 && marks <= 69){
+    const gradeLetter = 'A-';
+    console.log(gradeLetter)
+}
+else if ( marks >= 70 && marks <= 79){
+    const gradeLetter = 'A';
+    console.log(gradeLetter)
+}
+else  {
+    const gradeLetter = 'A+';
+    console.log(gradeLetter)
+}
+    
