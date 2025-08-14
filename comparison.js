72c9ee7b5c1eba0324b742e642935c428a524afe
Roman <romanhassan328@gmail.com>
2025-08-14 21:26:13 +0600

    added example comparison

---
+++ comparison.js
@@ -0,0 +1,32 @@
+// console.log(5>10);
+// console.log(5<10);
+// console.log(5==10);
+// console.log(10==10);
+// console.log(10>=10);
+// console.log(5>=10);
+// console.log(5<=10);
+// console.log(10==10.1);
+// console.log(5!=10);
+
+
+//-----------------------------------
+var guavaPrice = 50;
+var applePrice = 250;
+
+// console.log(guavaPrice > applePrice);
+// console.log(guavaPrice < applePrice);
+
+/**
+ *  1. ==
+ *  2. ===
+ */
+
+// console.log(10 == '10');
+// console.log(10 === '10');
+// console.log(10 === 'Tomato');
+
+// -----------------------------------
+console.log(10 != '10');
+console.log(10 !== '10');
+
+
